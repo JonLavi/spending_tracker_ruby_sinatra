ab2695f44872cdc955cf4e2ec1feac7de6987ad3
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-01-01 16:09:48 +0000

    Add merchant/new route and view

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -3,12 +3,28 @@ require( 'sinatra/contrib/all' )
 require_relative( '../models/merchant.rb' )
 also_reload( '../models/*' )
 
+#Show all Merchants
 get '/merchants' do
   @merchants = Merchant.all()
   erb ( :"merchants/index" )
 end
 
+#Create new Merchant
+get '/merchants/new' do
+  erb ( :"merchants/new" )
+end
+
+post '/merchants' do
+  new_merchant = Merchant.new(params)
+  new_merchant.save()
+  redirect to("/merchants")
+end
+
+#Show Merchant details
 get '/merchants/:id' do
   @merchant = Merchant.find(params['id'].to_i)
   erb( :"merchants/show" )
 end
+
+#Edit Merchant details
+#Delete Merchant
